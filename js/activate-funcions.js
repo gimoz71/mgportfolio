5a22586e9ce411c3bcc77fa4e1f88a27006e0bfd
gimoz71 <gianluca.monti@ecoseekr.com>
2014-02-16 12:30:31 +0100

    aggiunta sigla generation x

--- js/activate-funcions.js
+++ js/activate-funcions.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 							'</p>'
 	});
 	
-	/*$("a.grouped_elements_movie3").fancybox({
+	/*$("a.grouped_elements_movie4").fancybox({
 		'hideOnOverlayClick'	: true,
 		'width'			: 	640,
 		'height'		: 	480,
@@ -109,6 +109,38 @@ $(document).ready(function() {
 								'Formato Open:	<a href="images/gallery/3d/spot_springbac.ogv">"Ogg"</a>, <a href="images/gallery/3d/spot_springbac.webm">"WebM"</a>' +
 							'</p>'
 	});
+	
+	/*$("a.grouped_elements_movie3").fancybox({
+		'hideOnOverlayClick'	: true,
+		'width'			: 	720,
+		'height'		: 	576,
+		'type'			: 	'swf',
+		'swf'			: 	{
+			 'wmode'		: 'transparent',
+			'allowfullscreen'	: 'true'
+		}
+	});*/
+	
+	$("a.grouped_elements_movie3").fancybox({
+		'hideOnOverlayClick'	: true,
+		'width'			: 	720,
+		'height'		: 	576,
+		'content'		:	'<video width="720" height="576" controls autoplay>' +
+								'<source src="images/gallery/3d/generationx.mp4"  type="video/mp4" />' +
+								'<source src="images/gallery/3d/generationx.webm" type="video/webm" />' +
+								'<source src="images/gallery/3d/generationx.ogv"  type="video/ogg" />' +
+								'<object width="720" height="576" type="application/x-shockwave-flash" data="images/gallery/3d/player.swf">' +
+									'<param name="movie" value="images/gallery/3d/player.swf" />' +
+									'<param name="flashvars" value="controlbar=over&amp;image=images/gallery/3d/oldstuff.jpg&amp;file=generationx.mp4" />' +
+									'<img src="images/gallery/3d/generationx_1.jpg" width="720" height="576" alt="Old Stuff" title="Questo browser non &egrave; in grado di riprodurre il video, Scaricalo da un link qu&igrave; sotto" />' +
+								'</object>' +
+							'</video>' +
+							'<p><strong>Download Video:</strong>' +
+								'Formato proprietario:	<a href="images/gallery/3d/generationx.mp4">"MP4"</a> - ' +
+								'Formato Open:	<a href="images/gallery/3d/generationx.ogv">"Ogg"</a>, <a href="images/gallery/3d/generationx.webm">"WebM"</a>' +
+							'</p>'
+	});
+
 	
 	$("a.grouped_elements_flash1").fancybox({
 		'hideOnOverlayClick'	: true,
